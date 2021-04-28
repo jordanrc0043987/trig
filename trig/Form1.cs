a7d4cbe8d5ed9f0f9c3be22ccba69630407e289b
Jordan Cooper <RC0043987@student.riversidecollege.ac.uk>
2021-04-28 10:54:20 +0100

    Add project files.

---
+++ trig/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trig
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            double sin(double x)
+            {
+                return (Math.Sin(x * Math.PI / 180.0));
+            }
+
+            double asin(double x)
+            {
+                return (Math.Asin(x) * 180 / Math.PI); 
+
+            }
+        }
+    }
+}
